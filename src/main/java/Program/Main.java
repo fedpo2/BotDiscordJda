a88c971645b775317138b82b099b9f56e503d487
federico polidoro <federico.nicolas.polidoro@gmail.com>
2022-12-26 17:35:29 -0300

    Changes to be committed: 	new file:   src/main/java/Commands/CmdCopy.java 	renamed:    src/main/java/org/example/AudioPlayerSendHandler.java -> src/main/java/Libreria/AudioPlayerSendHandler.java 	renamed:    src/main/java/org/example/GuildMusicManager.java -> src/main/java/Libreria/GuildMusicManager.java 	renamed:    src/main/java/org/example/PlayerManager.java -> src/main/java/Libreria/PlayerManager.java 	renamed:    src/main/java/org/example/TrackManager.java -> src/main/java/Libreria/TrackManager.java 	renamed:    src/main/java/org/example/Main.java -> src/main/java/Program/Main.java
     Changes not staged for commit:
    	modified:   .gitignore
    	modified:   src/main/java/Commands/CmdCopy.java
    	modified:   src/main/java/Commands/CmdHelp.java
    	modified:   src/main/java/Commands/CmdPlay.java
    	modified:   src/main/java/Libreria/AudioPlayerSendHandler.java
    	modified:   src/main/java/Libreria/EventData.java
    	modified:   src/main/java/Libreria/GuildMusicManager.java
    	modified:   src/main/java/Libreria/PlayerManager.java
    	modified:   src/main/java/Libreria/TrackManager.java
    	modified:   src/main/java/Program/Main.java
    	modified:   src/main/resources/META-INF/MANIFEST.MF

--- src/main/java/Program/Main.java
+++ src/main/java/Program/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import Commands.CmdCopy;
 import Commands.CmdHelp;
 import Commands.CmdPing;
 import Commands.CmdPlay;
@@ -12,6 +13,7 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 public class Main extends ListenerAdapter {
+    CmdCopy copy = new CmdCopy();
     CmdPlay play = new CmdPlay();
     CmdPing ping = new CmdPing();
     CmdHelp help = new CmdHelp();
@@ -45,13 +47,14 @@ public class Main extends ListenerAdapter {
 
         System.out.println("cmd = " + ed.cmd);
         switch (ed.cmd) {
-            case "j": case "join" -> ed.am.openAudioConnection(ed.vc);
-            case "l": case "leave" -> leaveChannel(ed);
-            case "s": case "skip" -> play.execute(ed, true);
-            case "p": case "play" -> play.execute(ed, false);
-            case "pi": case "ping" -> ping.execute(ed);
-            case "h": case "help" -> help.execute(ed);
-            case "np" >
+            case "j": case "join": ed.am.openAudioConnection(ed.vc); break;
+            case "l": case "leave": leaveChannel(ed); break;
+            case "s": case "skip": play.execute(ed, true); break;
+            case "p": case "play": play.execute(ed, false); break;
+            case "pi": case "ping": ping.execute(ed); break;
+            case "h": case "help": help.execute(ed); break;
+            case "co" : case "copiame": copy.execute(ed);
+            //case "np"
 
         }
     }
